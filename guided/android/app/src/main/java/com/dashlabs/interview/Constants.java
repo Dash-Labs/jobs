b8437609fcab20818d3d577d716d138850918e85
blangel <langelb@gmail.com>
2017-05-15 13:59:42 -0400

    * Adding guided Android task

---
+++ guided/android/app/src/main/java/com/dashlabs/interview/Constants.java
@@ -0,0 +1,11 @@
+package com.dashlabs.interview;
+
+/**
+ * Created by cyuan on 5/11/17.
+ */
+
+public class Constants {
+
+    public static final String BASE_URL = "https://image.tmdb.org/t/p/w500/";
+
+}
